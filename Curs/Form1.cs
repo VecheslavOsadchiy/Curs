a83e939ad6d6264e070843e1ea526d20ac1caf1c
Балу Нескубидубин <osadachiy.02@mail.ru>
2022-01-10 16:46:35 +0800

    Появилась гравитация

--- Curs/Form1.cs
+++ Curs/Form1.cs
@@ -12,107 +12,45 @@ namespace Curs
 {
 
     public partial class Form1 : Form
+
     {
-        List<Particle> particles = new List<Particle>();
+        Emitter emitter = new Emitter(); // добавили эмиттер        
         public Form1()
         {
             InitializeComponent();
-
             // привязал изображение
-            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
-
-            
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);            
         }
-        private void UpdateState()
-        {
-            foreach (var particle in particles)
-            {
-                particle.Life -= 1; // уменьшаю здоровье
-                                    // если здоровье кончилось
-                if (particle.Life < 0)
-                {
-                    // восстанавливаю здоровье
-                    particle.Life = 20 + Particle.rand.Next(100);
-                    // перемещаю частицу в центр
-                    particle.X = MousePositionX;
-                    particle.Y = MousePositionY;
-
-                    // делаю рандомное направление, скорость и размер
-                    var direction = (double)Particle.rand.Next(360);
-                    var speed = 1 + Particle.rand.Next(10);
-
-                    particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
-                    particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
-                    particle.Radius = 2 + Particle.rand.Next(10);
-                }
-                else
-                {
-                    particle.X += particle.SpeedX;
-                    particle.Y += particle.SpeedY;
-                }
-            }
-            for (var i = 0; i < 10; ++i)
-            {
-                if (particles.Count < 500) // пока частиц меньше 500 генерируем новые
-                {
-                    var particle = new ParticleColorful();
-                    // ну и цвета меняем
-                    particle.FromColor = Color.Yellow;
-                    particle.ToColor = Color.FromArgb(0, Color.Magenta);
-                    particle.X = MousePositionX;
-                    particle.Y = MousePositionY;
-                    particles.Add(particle);
-                }
-                else
-                {
-                    break; // а если частиц уже 500 штук, то ничего не генерирую
-                }
-            }
-        }
-
-        private void Render(Graphics g)
-        {
-            // утащили сюда отрисовку частиц
-            foreach (var particle in particles)
-            {
-                particle.Draw(g);
-            }
-        }
-
-
-
         private void timer1_Tick(object sender, EventArgs e)
         {
-            UpdateState(); // каждый тик обновляем систему
+            emitter.UpdateState(); // каждый тик обновляем систему
             using (var g = Graphics.FromImage(picDisplay.Image))
             {
                 g.Clear(Color.Black); // добавил очистку              
-                Render(g); // рендерим систему
+                emitter.Render(g); // рендерим систему
             }
             // обновить picDisplay
             picDisplay.Invalidate();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
+        
+        
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
+        {// в обработчике заносим положение мыши в переменные для хранения положения мыши
+            emitter.MousePositionX = e.X;
+            emitter.MousePositionY = e.Y;
         }
-        private int MousePositionX = 0;
-        private int MousePositionY = 0;
-        private void Form1_MouseMove(object sender, MouseEventArgs e)
-        {
-            
-        }
-
         private void picDisplay_Click(object sender, EventArgs e)
         {
 
         }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
 
-        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
-        {// в обработчике заносим положение мыши в переменные для хранения положения мыши
-            MousePositionX = e.X;
-            MousePositionY = e.Y;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
